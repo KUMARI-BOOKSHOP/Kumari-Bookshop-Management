68ae10e05e22968a88d99841b11a9c93c6c85692
Sahan Nimesha <78093604+sahannimesh99@users.noreply.github.com>
2023-03-18 14:24:45 +0530

    Update http.py

--- pos/http.py
+++ pos/http.py
@@ -10,15 +10,13 @@ def create_app(config=Config):
 
 	db.init_app(app)
 
-	# app.register_blueprint(privilage.bp)
+	app.register_blueprint(privilage.bp)
+	app.register_blueprint(transactions.bp)
 	app.register_blueprint(products.bp)
 	app.register_blueprint(customers.bp)
 	app.register_blueprint(emploees.bp)
 	app.register_blueprint(suppliers.bp)
-	app.register_blueprint(transactions.bp)
-	# app.register_blueprint(invoice.bp)
 	with app.app_context():
 		db.create_all()
-		# print(app.url_map)
 		return app
 	
