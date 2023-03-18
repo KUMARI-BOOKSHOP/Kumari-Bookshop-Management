1e05d18c405a23284e57e0aacc554eb71b7b5911
Sahan Nimesha <78093604+sahannimesh99@users.noreply.github.com>
2023-03-18 13:46:58 +0530

    Update privilage.py

--- pos/models/privilage.py
+++ pos/models/privilage.py
@@ -2,6 +2,7 @@
 from pos.models import db
 
 class Privilage(db.Model):
-    holderId = db.Column(db.Integer, primary_key=True, autoincrement=True) 
-    holderName = db.Column(db.String(45), nullable=False)
+    register_id = db.Column(db.Integer, primary_key=True, autoincrement=True) 
+    username = db.Column(db.String(45), nullable=False, unique=True)
     password = db.Column(db.String(45), nullable=False)
+    
